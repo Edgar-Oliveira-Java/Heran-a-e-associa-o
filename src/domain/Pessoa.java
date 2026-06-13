e67e2b2654139717b2a78faef1674b2781e4ca78
Edgar Oliveira Souza <edgaroliveirasouza1@gmail.com>
2026-06-13 11:37:37 -0300

    Dando inicio ao projeto, criei a classe principal e modifiquei o git ignore

---
+++ src/domain/Pessoa.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class Pessoa {
+    private String name;
+    private int age;
+
+    public Pessoa(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
